8bd34d824bdc9686d3102aa7205bec90f8353604
Thu Anh Ma <tam.thuma@gmail.com>
2025-01-27 18:50:20 +0100

    allocated events to a separate event folder

--- index.js
+++ index.js
@@ -17,9 +17,19 @@ const client = new Client({
 
 client.login(token);
 
-client.once(Events.ClientReady, readyClient => { 
-    console.log(`Ready! Logged in as ${client.user.tag}`);
-});
+//loads the event files on startup
+const eventsPath = path.join(__dirname,"events");
+const eventFiles = fs.readdirSync(eventsPath).filter((file) => file.endsWith('.js'));
+
+for (const file of eventFiles){
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once){
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
 client.commands = new Collection();
 
@@ -40,27 +50,6 @@ for (const folder of commandFolders){
         }
     }
 }
-
-//Listener that creates an interaction event. 
-client.on(Events.InteractionCreate, async interaction => { 
-    if (!interaction.isChatInputCommand()) return;
-    console.log(interaction);
-    const command = interaction.client.commands.get(interaction.commandName);
-    if (!command){
-        console.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
-    try {
-        await command.execute(interaction);
-    } catch (error){
-        console.error(error);
-        if (interaction.replied || interaction.deferred){
-            await interaction.followUp( {content: 'There was an error while executing this command', flags: MessageFlags.Ephemeral});
-        } else {
-            await interaction.reply({content: 'There was an error while executing this command', flags: MessageFlags.Ephemeral});
-        }
-    }
-});
 
 
 
