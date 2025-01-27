cb3d699792aaa6f7fec018b09d76c58700836261
Thu Anh Ma <tam.thuma@gmail.com>
2025-01-27 15:44:44 +0100

    added commands folder and ping/pong command

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ const {token} = require('./config.json');
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.MessageContent
     ]
     
 });
 
+client.login(token);
+
 client.once(Events.ClientReady, readyClient => { 
-    console.log('Ready! Logged in as ${readyClient.user.tag}');
+    console.log(`Ready! Logged in as ${client.user.tag}`);
 });
 
-client.login(token);
+// client.on("messageCreate", msg => {
+//     if (msg.content === "ping"){
+//         console.log("Ping received!");
+//         msg.reply({content: 'pong'});
+//     }
+// });
+
 
