afc0ea6f6301d51c8f7e9a71ea90648d54b70db2
Thu Anh Ma <tam.thuma@gmail.com>
2025-02-10 16:01:45 +0100

    updated to show nickname instead of user name

--- commands/utility/leaderboard.js
+++ commands/utility/leaderboard.js
@@ -1,41 +1,49 @@
-const {SlashCommandBuilder} = require('discord.js');
-const {EmbedBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require("discord.js");
+const { EmbedBuilder } = require("@discordjs/builders");
 const profileModel = require("../../models/profileSchema");
 
 module.exports = {
-    data : new SlashCommandBuilder()
-        .setName('leaderboard')
-        .setDescription('Rank of members'),
-    async execute(interaction, profileData){
-        await interaction.deferReply();
-        const { username, id } = interaction.user;
-        const { crosses } = profileData;
+  data: new SlashCommandBuilder()
+    .setName("leaderboard")
+    .setDescription("Rank of members"),
+  async execute(interaction, profileData) {
+    await interaction.deferReply();
+    const { username, id } = interaction.user;
+    const { crosses } = profileData;
 
-        let leaderboardEmbed = new EmbedBuilder()
-            .setTitle("** Cross Leaderboard **")
-            .setColor(0x45d6fd)
-            .setFooter({text: "You are not ranked yet"});
-    
-        const members = await profileModel
-            .find()
-            .sort({crosses: -1})
-            .catch((err) => console.log(err));
-        
-        const memberIdx = members.findIndex((member) => member.userId === id);
-        leaderboardEmbed.setFooter({
-            text: `${username}, you have ${crosses} crosses`
-        });
-        
-        let description = "";
-        for (let i = 0; i < members.length; i++){
-            let {user} = await interaction.guild.members.fetch(members[i].userId);
-            if (!user) return;
-        let userCrosses = members[i].crosses;
-        description += `**${i + 1}. ${user.username}:** ${userCrosses} crosses \n`;
-        }
-        if (description !== ""){
-            leaderboardEmbed.setDescription(description);
-        }
-        await interaction.editReply({embeds: [leaderboardEmbed] });
-    },
+    //finding server nickname
+    const member = await interaction.guild.members.fetch(recievedUser.id);
+    const serverNickname = member.nickname || recievedUser.username;
+
+    let leaderboardEmbed = new EmbedBuilder()
+      .setTitle("** Cross Leaderboard **")
+      .setColor(0x45d6fd)
+      .setFooter({ text: "You are not ranked yet" });
+
+    const members = await profileModel
+      .find()
+      .sort({ crosses: -1 })
+      .catch((err) => console.log(err));
+
+    const memberIdx = members.findIndex((member) => member.userId === id);
+    leaderboardEmbed.setFooter({
+      text: `${serverNickname}, you have ${crosses} crosses`,
+    });
+
+    let description = "";
+    for (let i = 0; i < members.length; i++) {
+      let { user } = await interaction.guild.members.fetch(members[i].userId);
+
+      if (!user) return;
+      let userCrosses = members[i].crosses;
+      const serverNickname = user.displayName;
+      description += `**${
+        i + 1
+      }. ${serverNickname}:** ${userCrosses} crosses \n`;
+    }
+    if (description !== "") {
+      leaderboardEmbed.setDescription(description);
+    }
+    await interaction.editReply({ embeds: [leaderboardEmbed] });
+  },
 };
